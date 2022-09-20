0b4283b7115b2d5077be50ad31cbe91007477ed2
myoostis <parkgin2011@naver.com>
2022-09-20 17:16:09 +0900

    03

--- main.c
+++ main.c
@@ -6,16 +6,11 @@
 int main(void) {
 	
 	char text;
-	int i;
 	
-	printf("input a number : ");
+	printf("input a character : ");
 	scanf("%c", &text);
 	
-	i = text - '0' ;
-	printf("The input number is %i\n", i);
-	
-	
-	
+	printf("the next character of %c (%d) is %c (%d)\n", text, text, text+1, text+1 );
 	
 	return 0;
 }
