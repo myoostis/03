01db027b804e063a8de4120bfc81f82fb628e5a3
myoostis <parkgin2011@naver.com>
2022-09-20 17:08:35 +0900

    02

--- main.c
+++ main.c
@@ -5,17 +5,16 @@
 
 int main(void) {
 	
-	int input_int;
-	float input_float;
+	char text;
+	int i;
+	
+	printf("input a number : ");
+	scanf("%c", &text);
+	
+	i = text - '0' ;
+	printf("The input number is %i\n", i);
 	
 	
-	printf("enter an integer : ");
-	scanf("%d", &input_int);
-	
-	printf("enter a float : ");
-	scanf("%f", &input_float);
-	
-	printf("integer : %d, float : %f\n", input_int, input_float);
 	
 	
 	return 0;
